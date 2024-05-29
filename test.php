038e5fa8cd1b2d415d745d9bb44d3f15ad27ab86
EvilGribojui <andrey.fedorov90@gmail.com>
2024-05-29 23:44:36 +0300

    Добавил скрипт проверки соединения с базой (test.php). Как следствие поправил настройки подключения к базе.

--- test.php
+++ test.php
@@ -1,23 +1,13 @@
 <?php
-/*function rectangle($x, $y) {
-    $starsArray = array_fill(0, $x, '*');
-    $line = implode(' ', $starsArray);
-    for ($i = 0; $i < $y; $i++) {
-        echo $line."\n";
-    }
-}
+$dsn= "pgsql:host=localhost;dbname=tasks";
 
-rectangle(8,3);
-*/
-
-function number_of_days($x)
+try 
 {
-    $month= array("1"=>31,"2"=>28,"3"=>31, "4"=>30, "5"=>31, "6"=>30, "7"=>31,"8"=>31,"9"=>30,"10"=>31,"11"=>30,"12"=>31);
-    if (array_key_exists($x, $month)) {
-        echo $month[$x];
-    } else {
-        echo "Такого месяца нет";
-    }
+    $pdo =new PDO ($dsn, "postgres","");
+    echo "Ну здорово, отец!";
 }
-
+catch(PDOException $e) 
+{
+    die("Подключение не удалось :-(."."\n".$e->getMessage());
+}
 ?>
